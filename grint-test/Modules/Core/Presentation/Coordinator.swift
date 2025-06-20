2630785a4f1fc339a92b4abb11f42c02a5342666
Neptali Duque <nepta.din@gmail.com>
2025-06-19 20:15:45 -0400

    Changed folder structure in preparation for Detail view

---
+++ grint-test/Modules/Core/Presentation/Coordinator.swift
@@ -0,0 +1,10 @@
+//
+//  Coordinator.swift
+//  grint-test
+//
+//  Created by Neptali Duque on 6/19/25.
+//
+
+protocol Coordinator {
+    func goToController()
+}
--- grint-test/Modules/Home/Domain/Mapper/HomeDomainMapperImpl.swift
+++ grint-test/Modules/Home/Domain/Mapper/HomeDomainMapperImpl.swift
@@ -8,11 +8,13 @@
 struct HomeDomainMapperImpl: HomeDomainMapper {
     private enum Constants {
         static let amp = "amp;"
+        static let redditUrl = "https://www.reddit.com"
     }
 
     func domainToPresentation(_ value: RedditPostsEntry) -> UiRedditPostsEntry {
         let posts = value.posts.map {
             let cleansedThumbnail = $0.thumbnail?.replacingOccurrences(of: Constants.amp, with: "")
+            let completeDetailLink = Constants.redditUrl + $0.detailLink
             return UiRedditPost(
                 title: $0.title,
                 author: $0.author,
@@ -20,7 +22,7 @@ struct HomeDomainMapperImpl: HomeDomainMapper {
                 ups: $0.ups,
                 numComments: $0.numComments,
                 subreddit: $0.subreddit,
-                detailLink: $0.detailLink,
+                detailLink: completeDetailLink,
                 thumbnail: cleansedThumbnail,
                 size: $0.size
             )
